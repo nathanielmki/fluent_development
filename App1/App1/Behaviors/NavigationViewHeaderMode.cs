53cf6b08541f1b09ed034c304455c68e753fbf22
Nathaniel Maki <njmaki@mtu.edu>
2020-01-26 19:50:52 -0500

    First project commit

---
+++ App1/App1/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace App1.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
---
+++ App1/App1/Services/Ink/UndoRedo/IUndoRedoOperation.cs
@@ -0,0 +1,9 @@
+﻿namespace App1.Services.Ink.UndoRedo
+{
+    public interface IUndoRedoOperation
+    {
+        void ExecuteUndo();
+
+        void ExecuteRedo();
+    }
+}
